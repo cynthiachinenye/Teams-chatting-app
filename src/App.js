2f86f1d7e39a120137d0c77512f727af5627e5c8
cynthiachinenye <cynthiachiagu@gmail.com>
2024-01-10 14:52:51 +0100

    new

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {ChatEngine} from "react-chat-engine";
+import './App.css';
+
+
+const App = () =>{
+    return(
+        <ChatEngine
+        height="100vh"
+        projectID="7f1e2f07-abeb-4efc-977d-63150bf3e685"
+        userName="cyndi_bless"
+        userSecret="1234"
+
+        />
+    )
+}
